0be7b1d33935e5882eabf5cdf3bf03196bf4267a
Lucas <frelon99@gmail.com>
2024-10-23 09:19:46 +0200

    quest 3 on pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  return (
-  <div>
-    <PokemonCard/>
-  </div>
-  );
+	return (
+		<div>
+			<PokemonCard />
+		</div>
+	);
 }
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,27 @@
+
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
+
+
+
 function PokemonCard() {
-return <figure>
-<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
-<figcaption>Bulbasaur</figcaption>
-</figure>;
+	const pokemon = pokemonList;
+	return (
+		<figure>
+			{pokemon.imgSrc === null || pokemon.imgSrc === undefined ? <p>???</p> : <img src = {pokemon.imgSrc} alt= {pokemon.name}/>}
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+	);
 }
+
+
 
 export default PokemonCard;
